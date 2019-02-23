70b709935e47be6680de91eb7aae7e6f51a89797
sivayuges@gmail.com <sivayuges@gmail.com>
2019-02-23 01:12:58 +0000

    few changes

--- app/ClassStudent.php
+++ app/ClassStudent.php
@@ -4,8 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ClassStudent extends Model
-{
-  public $timestamps = true;
+class ClassStudent extends Model{
+  // public $timestamps = true;
 
 }
